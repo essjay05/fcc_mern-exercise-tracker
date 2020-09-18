89de01711f7feb0b78b58904639d5610f4e6a36a
Joy S <stepjoyous@gmail.com>
2020-09-17 17:48:29 -0700

    Successfully updated server to connect to MongoDB Atlas database.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Constants
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -10,6 +11,14 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// DB Connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log(`MongoDB database connection established successfully`);
+})
 
 // Port/Server Connection
 app.listen(PORT, () => {
