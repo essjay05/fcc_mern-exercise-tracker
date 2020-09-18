49d82198b5a77d9ffe3d901db0ee41035c00939a
Joy S <stepjoyous@gmail.com>
2020-09-18 11:46:03 -0700

    Created models folder in backend, created exercise.model.js and user.model.js.  Created user Schema to be used.

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+// Require constants
+const mongoose = require('mongoose');
+
+// Create Schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+}, {
+    timestamps: true,
+});
+
+// Create model Constant
+const User = mongoose.model( 'User', userSchema );
+
+// Export model Constant
+module.exports = User;
