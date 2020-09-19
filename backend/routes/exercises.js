e35a60b993aacddc9f29158b5ed968c5104f475f
Joy S <stepjoyous@gmail.com>
2020-09-19 11:39:43 -0700

    Updated exercises routes with read one, update, and delete.  Tested successfully on Postman and with MongoDB.

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -9,7 +9,13 @@ router.route('/').get((req, res) => {
         .then(exercises => res.json(exercises))
         .catch(err => res.status(400).json(`Error: ${err}`));
 })
+
 // Read One Exercise
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+})
 
 // Create Exercise
 router.route('/add').post((req, res) => {
@@ -31,8 +37,29 @@ router.route('/add').post((req, res) => {
 });
 
 // Update Exercise
+router.route('/update/:id').patch((req, res) => {
+    Exercise.findByIdAndUpdate(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+  
+            exercise.save()
+                .then(() => res.json(`Exercise successfully updated!`))
+                .catch(err => res.status(400).json(`Error: ${err}`));
+        })
+        .catch(err => res.status(400).json(`Error: ${err}`));
+
+})
 
 // Delete Exercise
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json(`Exercise deleted.`))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
 
 // Export routes
 module.exports = router;
