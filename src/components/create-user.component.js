d8fe7cca6c6c3a1af062ab9569e6c647ed337e59
Joy S <stepjoyous@gmail.com>
2020-09-23 17:50:14 -0700

    Implemented axios post into onSubmit function to create new user by connecting the front end and the backend.  Successfully created a new user from the front end form and connected it to the MongoDb backend.

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
 
@@ -37,6 +38,9 @@ export default class CreateUser extends Component {
         }
 
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
 
         this.setState({
             username: ''
